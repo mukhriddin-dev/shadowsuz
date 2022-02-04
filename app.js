826437780d5238b2487036c012b9329b9aec15d1
Akhmedov Web <ahmedovshohbaxt@gmail.com>
2022-02-04 23:19:17 +0500

    new

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const loader_hide=document.querySelector('.loader');
 const close=document.querySelector('.close');
+const bar=document.querySelector('.bar');
+const navbar=document.querySelector('.navbar_menu')
 close.addEventListener('click',()=>{
     loader_hide.classList.toggle('loader_hide')
 });
+bar.addEventListener('click',()=>{
+   navbar.classList.toggle('hide')
+})
