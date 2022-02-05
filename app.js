b43ba99c112f0303c27d7de21437982afd788e54
Akhmedov Web <ahmedovshohbaxt@gmail.com>
2022-02-05 16:09:03 +0500

    new

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const loader_hide=document.querySelector('.loader');
 const close=document.querySelector('.close');
 const bar=document.querySelector('.bar');
-const navbar=document.querySelector('.navbar_menu')
+const navbar=document.querySelector('.navbar_menu');
+const call=document.querySelectorAll('.call');
+const call_modal=document.querySelector('.call_modal')
+const call_close=document.querySelector('.call_modal_close')
+
 close.addEventListener('click',()=>{
-    loader_hide.classList.toggle('loader_hide')
+    loader_hide.classList.toggle('loader_hide');
 });
+
 bar.addEventListener('click',()=>{
-   navbar.classList.toggle('hide')
-})
+   navbar.classList.toggle('hide');
+});
+
+call.forEach(element =>
+    element.addEventListener('click',()=>{
+        call_modal.classList.toggle('call_hide');
+    }));
+
+call_close.addEventListener('click',()=>{
+    call_modal.classList.toggle('call_hide');
+});
