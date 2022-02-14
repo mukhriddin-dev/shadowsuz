2af7f79a37d7ec8deed0483b27468def1b69f124
Akhmedov Web <ahmedovshohbaxt@gmail.com>
2022-02-14 20:13:25 +0500

    text

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const close=document.querySelector('.close');
 const bar=document.querySelector('.bar');
 const navbar=document.querySelector('.navbar_menu');
 const call=document.querySelectorAll('.call');
-const call_modal=document.querySelector('.call_modal')
-const call_close=document.querySelector('.call_modal_close')
+const call_modal=document.querySelector('.call_modal');
+const call_close=document.querySelector('.call_modal_close');
+const search_icon=document.querySelectorAll('.carusel_icon');
+const model=document.querySelector('.model');
+const model_close=document.querySelector('.model_close');
+const registration=document.querySelector('.registration');
+const login_btn=document.querySelector('.login_btn');
 
 close.addEventListener('click',()=>{
     loader_hide.classList.toggle('loader_hide');
@@ -21,4 +26,15 @@ call.forEach(element =>
 
 call_close.addEventListener('click',()=>{
     call_modal.classList.toggle('call_hide');
+});
+
+search_icon.forEach(element=>
+element.addEventListener('click',()=>{
+    model.classList.toggle('model_hide')
+}));
+model_close.addEventListener('click',()=>{
+    model.classList.toggle('model_hide')
+});
+login_btn.addEventListener('click',()=>{
+    registration.classList.toogle('registration_hide')
 });
